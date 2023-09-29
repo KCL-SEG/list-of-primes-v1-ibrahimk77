dcc7a6913df2d80d1cfa87516ac5132f98ee0f3d
Ibrahim <k22013981@nmslab-005828.nms.kcl.ac.uk>
2023-09-29 15:05:14 +0100

    Solution

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes == 0:
+        return list
+
+    num = 2
+    list.append(num)
+
+    while(len(list) < number_of_primes):
+
+        prime = True
+        num+=1
+        for i in list:
+            if num%i == 0:
+                prime = False
+            
+        if prime:
+            list.append(num)
+        
+
+
+    
     return list
